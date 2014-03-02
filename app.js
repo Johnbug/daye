c7188178bce2e6305124f6842ee6497d753cb55a
xsbailong <xsbailong@gmail.com>
2014-03-02 19:53:15 +0800

    修改模板由jade为ejs

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var path = require('path');
 var MongoStore = require('connect-mongo')(express);
 var settings = require('./settings');
 var flash = require('connect-flash');
+var partials=require('express-partials');
 
 var app = express();
 
@@ -17,12 +18,13 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 app.use(flash());
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(partials());
 
 app.use(express.cookieParser());
 app.use(express.session({
