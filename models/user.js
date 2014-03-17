0bf872287ce3b254e4d2de5271ea539dc3d48938
ziyu.zzy <ziyu.zzy@alibaba-inc.com>
2014-03-17 13:03:30 +0800

    测试使用thenjs

--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
 /**
  * Created by jonbug on 13-12-8.
  */
-var mongodb = require('./db');
+var mongodb = require('./db'),
+    thenjs = require("thenjs");
 
 function User(user){
     this.name = user.name;
@@ -50,6 +51,7 @@ User.get = function(name, callback){//读取用户信息
             return callback(err);
         }
         //读取 users 集合
+        /*
         db.collection('users', function(err, collection){
             if(err){
                 return callback(err);
@@ -67,6 +69,35 @@ User.get = function(name, callback){//读取用户信息
                 }
             });
         });
+        */
+        //thenjs 异步编程，减少回调嵌套，而改用链式，易于代码维护
+        //此branch用于测试thenjs
+        thenjs(function(defer){
+            db.collection('users',function(err,collection){
+                //console.log(collection);
+                defer(err,collection);
+            });
+        })
+            .then(function(defer,collection){
+                //console.log()
+                collection.findOne({
+                    name:name
+                },function(err,doc){
+                    //console.log(doc);
+                    defer(err,doc);
+                })
+            },function(defer,err){
+                callback(err);
+            })
+            .then(function(err,doc){
+                pool.release(db);
+                if(doc){
+                    var user = new User(doc);
+                    callback(err, user);//成功！返回查询的用户信息
+                } else {
+                    callback(err, null);//失败！返回null
+                }
+            })
     })
 };
 
